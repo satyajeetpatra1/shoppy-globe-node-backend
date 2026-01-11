f05e4ffcfa1e5fdf9466ec06384df5c856103711
satyajeetpatra1 <satyajeetpatra.dev@gmail.com>
2026-01-11 22:19:44 +0530

    Cart started and respective Model added

---
+++ models/Cart.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const cartSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+export default mongoose.model("Cart", cartSchema);
