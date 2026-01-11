f4f3e92babb5c7f0439ce206c7ba149b455a09cf
satyajeetpatra1 <satyajeetpatra.dev@gmail.com>
2026-01-11 16:53:32 +0530

    basic boiler plate code added in index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 import express from "express";
 
-const app = express()
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
